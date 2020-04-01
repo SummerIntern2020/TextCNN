bd9f4ca5eafabbc3e0930b38b41b876206b2f14a
lzhangbq <55970262+lzhangbq@users.noreply.github.com>
2020-04-01 17:54:00 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,24 +1,33 @@
-def TextCNN(num_classes, seq_length, embedding_dims, filter_sizes, num_filters, last_activation = 'softmax',l2_reg_lambda = 0.0,model_img_path=None):
-        input = Input((seq_length,))
-        logging.info("x_input.shape: %s" % str(x_input.shape))
-        embedding = Embedding(imput_dim = num_classes, output_dim = embedding_dims, input_length = seq_length)(input)
-        convs = []
-        for kernel_size in filter_sizes:
-            C = Conv1D(128, kernel_size, activation='relu')(embedding)
-            M = GlobalMaxPooling1D()(C)
-            convs.append(M)
-            logging.info("kernel_size: %s \t c.shape: %s \t p.shape: %s" % (kernel_size, str(C.shape), str(M.shape)))
-        convs = concatenate([p for p in convs])
-        logging.info("pool_output.shape: %s" % str(convs.shape))
-        flat = Flatten()(convs)
-        dropping = Dropout(0.2)(flat)
-        y = Dense(output_dim, activation='softmax')(dropping)
-        model = Model([x_input], outputs=[y])
-        if model_img_path:
-            plot_model(model, to_file=model_img_path, show_shapes=True, show_layer_names=False)
-        model.summary()
-        return model
-
-        output = Dense(num_classes, activation=last_activation)(x)
-        model = Model(inputs=input, outputs=output)
-        return model
+def TextCNN_model(x_train_padded_seqs,y_train,x_test_padded_seqs,y_test):
+    # 模型结构：词嵌入-卷积池化*3-拼接-全连接-dropout-全连接
+    main_input = Input(shape=(300,), dtype='float64')
+    # 词嵌入（使用预训练的词向量）
+    embedder = Embedding(len(vocab) + 1, 300, input_length=50, trainable=False)
+    #embedder = Embedding(len(vocab) + 1, 300, input_length=50, trainable=False)
+    embed = embedder(main_input)
+    # 词窗大小分别为3,4,5
+    cnn1 = Conv1D(256, 3, padding='same', strides=1, activation='relu')(embed)
+    cnn1 = GlobalMaxPooling1D()(cnn1)
+    cnn2 = Conv1D(256, 4, padding='same', strides=1, activation='relu')(embed)
+    cnn2 = GlobalMaxPooling1D()(cnn2)
+    cnn3 = Conv1D(256, 5, padding='same', strides=1, activation='relu')(embed)
+    cnn3 = GlobalMaxPooling1D()(cnn3)
+    # 合并三个模型的输出向量
+    cnn = Concatenate()([cnn1, cnn2, cnn3])
+    flat = Flatten()(cnn)
+    drop = Dropout(0.2)(flat)
+    main_output = Dense(43, activation='softmax')(drop)
+    model = Model(inputs=main_input, outputs=main_output)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+ 
+    one_hot_labels = to_categorical(y_train, num_classes=43)  # 将标签转换为one-hot编码
+    model.fit(x_train_padded_seqs, one_hot_labels, batch_size=800, epochs=5)
+    result = model.predict(x_test_padded_seqs)  # 预测样本属于每个类别的概率
+    result_labels = np.argmax(result, axis=1)  # 获得最大概率对应的标签
+    y_predict = list(map(str, result_labels))
+    print('准确率', metrics.accuracy_score(y_test, y_predict))
+    print('平均f1-score:', metrics.f1_score(y_test, y_predict, average='weighted'))
+        
+        
+        
+TextCNN_model(x_train, y_train, x_test, y_test)
